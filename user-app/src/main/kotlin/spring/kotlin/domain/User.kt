7b1faf71a0f21bb439d24106f03278ed84e38975
E212406B <pacome.cailleteau@univ-nantes.fr>
2023-12-07 10:51:57 +0100

    Erreurs HTTP ajoutées

--- user-app/src/main/kotlin/spring/kotlin/domain/User.kt
+++ user-app/src/main/kotlin/spring/kotlin/domain/User.kt
@@ -3,11 +3,11 @@ package spring.kotlin.domain
 import java.time.LocalDate
 
 data class User(
-        var email: String,
-        var nom: String,
-        var adresseDeLivraison: String,
-        var estAbonnee: Boolean,
-        var dateDerniereCommande: LocalDate?,
-        var age: Int
+    var email: String,
+    var nom: String,
+    var adresseDeLivraison: String,
+    var estAbonnee: Boolean,
+    var dateDerniereCommande: LocalDate?,
+    var age: Int
 ) {
 }
--- user-app/src/main/kotlin/spring/kotlin/repository/entity/UserEntity.kt
+++ user-app/src/main/kotlin/spring/kotlin/repository/entity/UserEntity.kt
@@ -9,12 +9,12 @@ import java.time.LocalDate
 @Entity
 @Table(name = "users")
 class UserEntity(
-        @Id val email: String,
-        val nom: String,
-        val adresseDeLivraison: String,
-        val estAbonnee: Boolean,
-        val dateDerniereCommande: LocalDate?,
-        val age: Int
+    @Id val email: String,
+    val nom: String,
+    val adresseDeLivraison: String,
+    val estAbonnee: Boolean,
+    val dateDerniereCommande: LocalDate?,
+    val age: Int
 ) {
     fun asUser() =
         User(this.email, this.nom, this.adresseDeLivraison, this.estAbonnee, this.dateDerniereCommande, this.age)
